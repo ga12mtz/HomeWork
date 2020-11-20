b945ec82e0494eb36dd2450e8111f57e96479b59
ga12mtz <74768111+ga12mtz@users.noreply.github.com>
2020-11-20 13:18:13 +0300

    Create 1.cs

---
+++ 1.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Homework
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name:");
+            string Name = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + Name);
+        }
+    }
+}
