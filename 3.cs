e9a477ec2f9143aac7634418d5d63159bfc9a2dd
ga12mtz <74768111+ga12mtz@users.noreply.github.com>
2020-11-21 14:48:38 +0300

    Create 3.cs

---
+++ 3.cs
@@ -0,0 +1,12 @@
+using System.IO;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        char c = (char)Console.Read();
+        char v = (char)(c+1);
+        Console.WriteLine(v);
+    }
+}
